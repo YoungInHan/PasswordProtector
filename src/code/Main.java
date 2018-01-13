1a641987b4df603cb3077f30d2310bb5e024d9fb
samuelnoguchi <33236743+samuelnoguchi@users.noreply.github.com>
2018-01-12 21:06:56 -0500

    Connected main to database

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,10 +1,29 @@
 package code;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws Exception{
+		getConnection();
 
 	}
+	
+	public static Connection getConnection()throws Exception {
+		try{
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/passwordprotector";
+			String username = "root";
+			String password = "caliorbust";
+			Class.forName(driver);
+			
+			Connection conn = DriverManager.getConnection(url,username,password);
+			System.out.println("Connected");
+			return conn;
+		}catch(Exception e){System.out.println(e);}
+		return null;
+	}
+	
 
 }
